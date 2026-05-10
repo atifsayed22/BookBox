f1a8bb26dd942c2d52d7f693073b97c90dbb75ed
atifsayed22 <sayedatif4321@gmail.com>
2026-05-10 18:29:15 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin:process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials:true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
